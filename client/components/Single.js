220df99fa679d0d75433545897153afc8653692b
samirrajpurkar <samir.rajpurkar@gmail.com>
2016-10-28 23:56:41 +0400

    single recipe component

---
+++ client/components/Single.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Single = React.createClass({
+  render() {
+    return (
+      <div className="single-recipe">
+        I am in Single
+      </div>
+    )
+  }
+})
+
+export default Single;
