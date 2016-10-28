2e25aeabe638a37cc865568a6f46e2e693e47004
samirrajpurkar <samir.rajpurkar@gmail.com>
2016-10-28 23:56:28 +0400

    recipe grid component

---
+++ client/components/RecipeGrid.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+const RecipeGrid = React.createClass({
+  render() {
+    return (
+      <div className="recipe-grid">
+        Im in the Recipe-grid
+      </div>
+    )
+  }
+})
+
+export default RecipeGrid;
