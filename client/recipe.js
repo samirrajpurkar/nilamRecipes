f15e8beffe122872fb37c7148d3df87c10254be8
samirrajpurkar <samir.rajpurkar@gmail.com>
2016-10-28 23:56:59 +0400

    handles routing

--- client/recipe.js
+++ client/recipe.js
@@ -1,2 +1,25 @@
 import React from 'react';
+import {render} from 'react-dom';
+
+//Import css
+import css from './styles/style.styl';
+
+//Import React components
+import Main from './components/Main';
+import Single from './components/Single';
+import RecipeGrid from './components/RecipeGrid';
+
+//Import React Router deps
+import {Router, Route, IndexRoute,browserHistory} from 'react-router';
+
+const router = (
+    <Router history={browserHistory}>
+      <Route path="/" component={Main}>
+        <IndexRoute component={RecipeGrid}></IndexRoute>
+        <Route path="view/:recipeId" component={Single}></Route>
+      </Route>
+    </Router>
+  )
+
+render(router, document.getElementById('root'));
 
