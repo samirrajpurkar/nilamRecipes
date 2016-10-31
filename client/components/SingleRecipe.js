37afcba1d669217eb33c7e88b0b6e1468708d470
samirrajpurkar <samir.rajpurkar@gmail.com>
2016-10-31 21:19:44 +0400

    Single Recipe

---
+++ client/components/SingleRecipe.js
@@ -0,0 +1,48 @@
+import React from 'React';
+import {Link} from 'react-router';
+import CSSTransitionGroup from 'react-addons-css-transition-group';
+
+const SingleRecipe = React.createClass({
+  render() {
+    const {recipe, i, comments} = this.props;
+
+    return(
+      <figure className="grid-figure">
+
+        <div className="grid-recipe-wrap">
+          
+          <Link to={'/view/${recipe.code}'}>
+            <img src={recipe.display_src} alt={recipe.caption} />
+          </Link>
+
+          <CSSTransitionGroup transitionName="like" transitionEnterTimeout={500} transitionLeaveTimeout={500}>
+            <span key={recipe.likes} className="likes-heart">
+              {recipe.likes}
+            </span>
+          </CSSTransitionGroup>
+        
+        </div>
+
+        <figcaption>
+          <p>{recipe.caption}</p>
+
+          <div className="control-buttons">
+            <button className="likes">
+              &hearts;{recipe.likes}
+            </button>
+            <Link className="button" to={'/view/${recipe.code}'}>
+              <span className="comment-count">
+                <span className="speech-bubble">
+                </span>
+                {comments[recipe.code] ? comments[recipe.code].length : 0}
+              </span>
+            </Link>
+          </div>
+        </figcaption>
+        
+      </figure>
+    )
+  }
+})
+
+export default SingleRecipe;
