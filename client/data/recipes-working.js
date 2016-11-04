fe39f0b605eb35bfa196089cad2583f015a5417a
samirrajpurkar <samir.rajpurkar@gmail.com>
2016-11-04 19:49:24 +0400

    recipes data referencing local images

---
+++ client/data/recipes-working.js
@@ -0,0 +1,12 @@
+const recipes = [
+   {
+      "code":"BAcyDyQwcXX",
+      "caption":"Lunch #hamont",
+      "likes":56,
+      "id":"1161022966406956503",
+      "display_src":'../../images/IMG_0837.jpg'
+   }
+];
+
+
+export default recipes;
